8dcd10162b3c5b91f0595ba2d99dcfbad64842db
Ugo Ridolfi <skynet.blockchain.lab@gmail.com>
2021-11-16 11:47:17 +0100

    fix outdated messages

--- skynet/cmds/init_funcs.py
+++ skynet/cmds/init_funcs.py
@@ -88,10 +88,14 @@ def check_keys(new_root: Path, keychain: Optional[Keychain] = None) -> None:
             if all_targets[-1] == config["pool"].get("xnt_target_address"):
                 stop_searching_for_pool = True
 
-    # Set the destinations
+    # Set the destinations, if necessary
+    updated_target: bool = False
     if "xnt_target_address" not in config["farmer"]:
-        print(f"Setting the xnt destination address fees reward to {all_targets[0]}")
+        print(
+            f"Setting the xnt destination for the farmer reward (1/8 plus fees, solo and pooling) to {all_targets[0]}"
+        )
         config["farmer"]["xnt_target_address"] = all_targets[0]
+        updated_target = True
     elif config["farmer"]["xnt_target_address"] not in all_targets:
         print(
             f"WARNING: using a farmer address which we don't have the private"
@@ -102,8 +106,9 @@ def check_keys(new_root: Path, keychain: Optional[Keychain] = None) -> None:
     if "pool" not in config:
         config["pool"] = {}
     if "xnt_target_address" not in config["pool"]:
-        print(f"Setting the xnt pool destination address reward to {all_targets[0]}")
+        print(f"Setting the xnt destination address for pool reward (7/8 for solo only) to {all_targets[0]}")
         config["pool"]["xnt_target_address"] = all_targets[0]
+        updated_target = True
     elif config["pool"]["xnt_target_address"] not in all_targets:
         print(
             f"WARNING: using a pool address which we don't have the private"
@@ -121,6 +126,12 @@ def check_keys(new_root: Path, keychain: Optional[Keychain] = None) -> None:
             f"WARNING: using a timelord address which we don't have the private"
             f" keys for. We searched the first {number_of_ph_to_search} addresses. Consider overriding "
             f"{config['timelord']['xnt_target_address']} with {all_targets[0]}"
+        )
+
+    if updated_target:
+        print(
+            f"To change the XNT destination addresses, edit the `xnt_target_address` entries in"
+            f" {(new_root / 'config' / 'config.yaml').absolute()}."
         )
 
     # Set the pool pks in the farmer
