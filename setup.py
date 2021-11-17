d260d5bd75c4fc6476738ff15630a4b96ff87662
Ugo Ridolfi <skynet.blockchain.lab@gmail.com>
2021-11-17 04:48:01 +0100

    setup updates

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ dependencies = [
     "skynetbip158==1.1.dev3",  # bip158-style wallet filters
     "skynetpos==1.0.4.dev14",  # proof of space
     "clvm==0.9.7",
-    "clvm_rs==0.1.13",
+    "clvm_rs==0.1.14",
     "clvm_tools==0.4.3",
     "aiohttp==3.7.4",  # HTTP server for full node rpc
     "aiosqlite==0.17.0",  # asyncio wrapper for sqlite, to store blocks
