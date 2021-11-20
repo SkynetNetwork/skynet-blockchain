8e0aa695ef0f013f9e44c8f179538a6fd30e53fd
Ugo Ridolfi <skynet.blockchain.lab@gmail.com>
2021-11-20 21:56:54 +0100

    tests CAT changes

--- tests/core/full_node/ram_db.py
+++ tests/core/full_node/ram_db.py
@@ -6,6 +6,7 @@ from skynet.consensus.blockchain import Blockchain
 from skynet.consensus.constants import ConsensusConstants
 from skynet.full_node.block_store import BlockStore
 from skynet.full_node.coin_store import CoinStore
+from skynet.full_node.hint_store import HintStore
 from skynet.util.db_wrapper import DBWrapper
 
 
@@ -14,5 +15,6 @@ async def create_ram_blockchain(consensus_constants: ConsensusConstants) -> Tupl
     db_wrapper = DBWrapper(connection)
     block_store = await BlockStore.create(db_wrapper)
     coin_store = await CoinStore.create(db_wrapper)
-    blockchain = await Blockchain.create(coin_store, block_store, consensus_constants)
+    hint_store = await HintStore.create(db_wrapper)
+    blockchain = await Blockchain.create(coin_store, block_store, consensus_constants, hint_store)
     return connection, blockchain
